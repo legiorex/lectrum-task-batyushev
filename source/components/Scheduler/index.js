cb849cbeb654df9e38737494c362a85955115f2c
legiorex <batyushev@gmail.com>
2018-11-17 00:08:03 +1000

    Правка ментора - сделал один компонент Scheduler

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -1,11 +1,6 @@
 // Core
 import React, { Component } from 'react';
 
-// Components
-import Header from 'components/Header';
-import Content from 'components/Content';
-import Footer from 'components/Footer';
-
 // Instruments
 import Styles from './styles.m.css';
 import { api } from '../../REST'; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
@@ -15,12 +10,53 @@ export default class Scheduler extends Component {
         return (
             <section className = { Styles.scheduler }>
                 <main>
-                    <Header />
-                    <Content />
-                    <Footer />
+                    <header>
+                        <h1>Планировщик задач</h1>
+                        <input placeholder= 'Поиск' type= 'search' value= '' />
+                    </header>
+                    <section>
+                        <form>
+                            <input
+                                maxLength= '50'
+                                placeholder ='Описaние моей новой задачи'
+                                type ='text'
+                                value= ''
+                            />
+                            <button>Добавить задачу</button>
+                        </form>
+                        <div>
+                            <ul>
+                                <div />
+                            </ul>
+                        </div>
+                    </section>
+                    <footer>
+                        <div>
+                            <svg version ='1.1' viewBox= '0 0 27 27'>
+                                <g>
+                                    <rect
+                                        fill ='#363636'
+                                        height= '25'
+                                        rx= '5'
+                                        ry ='5'
+                                        stroke= '#363636'
+                                        width ='25'
+                                        x= '1'
+                                        y= '1'
+                                    />
+                                    <path
+                                        d= 'M22.12 6c-3.12 3.16-6.84 6.36-10.23 9.64l-5.42-4.05L4 14.84l6.78 5.08L12.23 21l1.25-1.25C17 16.2 21.29 12.6 25 8.89z'
+                                        fill= '#fff'
+                                    />
+                                </g>
+                            </svg>
+                        </div>
+                        <span className ={ Styles.completeAllTasks }>
+                            Все задачи выполнены
+                        </span>
+                    </footer>
                 </main>
             </section>
         );
     }
 }
-console.log('test');
